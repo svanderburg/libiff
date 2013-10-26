763e42e04f40ca6684e62518f30ec6de7dcfa4fc
Sander van der Burg <svanderburg@gmail.com>
2013-10-26 21:32:56 +0200

    Added release Nix expression and fix test automake file to include all required headers

---
+++ release.nix
@@ -0,0 +1,78 @@
+{ nixpkgs ? <nixpkgs>
+, systems ? [ "i686-linux" "x86_64-linux" ]
+, buildForAmiga ? false
+, amigaosenvPath ? <amigaosenv>
+}:
+
+let
+  pkgs = import nixpkgs {};
+  
+  version = builtins.readFile ./version;
+  
+  jobs = rec {
+    tarball =
+      { libiff ? {outPath = ./.; rev = 1234;}
+      , officialRelease ? false
+      }:
+
+      with pkgs;
+
+      releaseTools.sourceTarball {
+        name = "libiff-tarball";
+        src = libiff;
+        inherit version officialRelease;
+
+        buildInputs = [];
+      };
+      
+    build =
+      { tarball ? jobs.tarball {} }:
+      
+      (pkgs.lib.genAttrs systems (system:
+        with import nixpkgs { inherit system; };
+        
+        releaseTools.nixBuild {
+          name = "libiff";
+          inherit version;
+          src = tarball;
+        }
+      )) //
+      (pkgs.lib.optionalAttrs (buildForAmiga)
+        (let
+          amigaosenv = import amigaosenvPath {
+            inherit (pkgs) stdenv uae procps;
+            lndir = pkgs.xorg.lndir;
+          };
+        in
+        {
+          m68k-amigaos.lib = amigaosenv.mkDerivation {
+            name = "libiff-${version}";
+            src = "${tarball}/tarballs/libiff-${version}pre1234.tar.gz";
+      
+            buildCommand = ''
+              tar xfvz $src
+              cd libiff-${version}pre1234
+              CFLAGS='-noixemul -O3' ./configure --prefix=/OUT --disable-shared
+              cd src/libiff
+              make
+              make install
+            '';
+          };
+        
+          m68k-amigaos.tools = amigaosenv.mkDerivation {
+            name = "libiff-${version}";
+            src = "${tarball}/tarballs/libiff-${version}pre1234.tar.gz";
+      
+            buildCommand = ''
+              tar xfvz $src
+                cd libiff-${version}pre1234
+               ./configure --prefix=/OUT --disable-shared
+               make
+               make check
+               make install
+            '';
+          };
+        }));
+  };
+in
+jobs
